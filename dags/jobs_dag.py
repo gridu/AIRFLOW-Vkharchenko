86c7a3a9a4b42b43f48020b9311eccb316b4c923
vkharchenko <vkharchenko@griddynamics.com>
2020-03-20 20:53:48 +0200

    Code practice: add SubDAG and Variables in trigger DAG

--- dags/jobs_dag.py
+++ dags/jobs_dag.py
@@ -32,13 +32,24 @@ def check_table_exist(**kwargs):
         return 'create_table'
 
 
+def push_run_id(**kwargs):
+    return '{run_id} ended'.format(run_id=kwargs['run_id'])
+
+
+def pull_the_result(**kwargs):
+    ti = kwargs['ti']
+    msg = ti.xcom_pull(task_ids='query_the_table', key='return_value')
+    print("received message: '%s'" % msg)
+    context = ti.get_template_context()
+    print("context: {}".format(context))
+
+
 for config_key, config_value in config.items():
     def create_dag():
         with DAG(
                 config_key,
                 default_args={'start_date': config_value['start_date']},
                 schedule_interval=config_value['schedule_interval']) as dag:
-
             task_log_info = PythonOperator(
                 task_id='log_info',
                 provide_context=False,
@@ -64,11 +75,19 @@ for config_key, config_value in config.items():
                 task_id='insert_new_row',
                 trigger_rule='none_failed')
 
-            task_query_the_table = DummyOperator(
-                task_id='query_the_table')
+            task_query_the_table = PythonOperator(
+                task_id='query_the_table',
+                python_callable=push_run_id,
+                provide_context=True)
+
+            task_print_the_result = PythonOperator(
+                task_id='print_the_result',
+                python_callable=pull_the_result,
+                provide_context=True)
 
             task_log_info >> task_echo_username_bash >> task_check_table_exist >> \
-            [task_skip_table_creation, task_create_table] >> task_insert_new_row >> task_query_the_table
+            [task_skip_table_creation, task_create_table] >> task_insert_new_row >> task_query_the_table >> \
+            task_print_the_result
 
         return dag
 
